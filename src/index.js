ff0a5835be5b32c8a974f7c5e3a3b563c233f60e
Alex Andras Mandrik <mndalex@protonmail.com>
2022-03-01 09:52:43 +0100

    Create the sticky-sidebar directive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-export default (Alpine) => {};
+export default (Alpine) => {
+    Alpine.directive("sticky-sidebar", (el, { modifiers }, { cleanup }) => {
+        const modifiersObject = {};
+
+        for (let index = 0; index < modifiers.length; index += 2) {
+            modifiersObject[modifiers[index]] = Number(modifiers[index + 1]);
+        }
+
+        const top = modifiersObject.top || 0;
+
+        const bottom = modifiersObject.bottom || 0;
+
+        const minScreenWidth = modifiersObject.min_screen_width || 0;
+
+        const layoutHeight = top + bottom;
+
+        const defaultClassList = el.classList;
+
+        el.classList.add("sticky", "transition-[top]");
+
+        el.style.top = `${top}px`;
+
+        let lastScrollTop = window.scrollTop();
+
+        const handler = () => {
+            if (window.innerWidth < minScreenWidth) {
+                el.style.top = null;
+
+                el.classList = defaultClassList;
+
+                return;
+            }
+
+            const windowHeight = window.innerHeight - layoutHeight;
+
+            if (windowHeight > el.offsetHeight) {
+                el.style.top = `${top}px`;
+                return;
+            }
+
+            const scrollTop = window.scrollTop();
+
+            if (scrollTop > lastScrollTop) {
+                el.style.top = `${top + (windowHeight - el.offsetHeight)}px`;
+            } else {
+                el.style.top = `${top}px`;
+            }
+
+            lastScrollTop = scrollTop;
+        };
+
+        document.addEventListener("scroll", handler);
+
+        cleanup(() => {
+            document.removeEventListener("scroll", handler);
+        });
+    });
+};
